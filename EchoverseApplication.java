c0397f048a84a58a885b4e68e715d1b252f95494
Snehil Joshi <128805122+Snehij523@users.noreply.github.com>
2025-04-20 17:08:44 +0530

    Add files via upload

---
+++ EchoverseApplication.java
@@ -0,0 +1,13 @@
+package com.echoverse.echoverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EchoverseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EchoverseApplication.class, args);
+	}
+
+}
